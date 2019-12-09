1fc79e33e86553f4042260d85c31ea5cbb9b04fd
Attila Boros <boros.attila17@gmail.com>
2019-12-09 10:52:54 +0100

    try to log cards and current_buy in

--- Player.js
+++ Player.js
@@ -5,6 +5,11 @@ class Player {
 
   static betRequest(gameState, bet) {
     console.log(gameState);
+    let cards = gameState.getAttribute("hole_cards") ;
+    let attribute = gameState.currentBuyIn.value;
+    console.log("current_buy_in:" + attribute);
+    console.log("cards:" +cards);
+
     bet(0);
   }
 
