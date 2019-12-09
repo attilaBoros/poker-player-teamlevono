aabf4c6ab3a766ac10449bb437f68a5b547fc739
Attila Boros <boros.attila17@gmail.com>
2019-12-09 11:31:25 +0100

    mehet

--- Player.js
+++ Player.js
@@ -1,16 +1,19 @@
 class Player {
   static get VERSION() {
-    return '0.7';
+    return '0.8';
   }
 
   static betRequest(gameState, bet) {
     console.log(gameState);
 
     let attribute = gameState.current_buy_in;
+    let minimumRaise = gameState.minimum_raise;
     console.log("current_buy_in:" + attribute);
-    const currentBuyIn = parseInt(attribute);
 
-    bet(currentBuyIn + 5);
+    const currentBuyIn = parseInt(attribute);
+    const minimumR = parseInt(minimumRaise);
+
+    bet(currentBuyIn + minimumR);
   }
 
   static showdown(gameState) {
