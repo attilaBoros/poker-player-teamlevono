e59813f345fe4bd1bf536d1d140e04cd071eed46
Attila Boros <boros.attila17@gmail.com>
2019-12-09 10:56:06 +0100

    ghotfix GetAttribute

--- Player.js
+++ Player.js
@@ -1,14 +1,12 @@
 class Player {
   static get VERSION() {
-    return '0.3';
+    return '0.4';
   }
 
   static betRequest(gameState, bet) {
     console.log(gameState);
-    let cards = gameState.getAttribute("hole_cards") ;
     let attribute = gameState.currentBuyIn.value;
     console.log("current_buy_in:" + attribute);
-    console.log("cards:" +cards);
 
     bet(0);
   }
