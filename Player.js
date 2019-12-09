c413f943e704ca39051c5d79262291081b349633
MelathStofi <melath.stofi@gmail.com>
2019-12-09 11:34:07 +0100

    sdas

--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
 class Player {
   static get VERSION() {
-    return '0.7';
+    return '0.8';
   }
 
   static betRequest(gameState, bet) {
@@ -9,6 +9,7 @@ class Player {
     let attribute = gameState.current_buy_in;
     console.log("current_buy_in:" + attribute);
     const currentBuyIn = parseInt(attribute);
+    console.log("hole_cards: " + gameState.hole_cards)
 
     bet(currentBuyIn + 5);
   }
